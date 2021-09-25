47a7273cce2ba3d53126862806513c3518f3b1a2
naritayuuto <yuuto122narita@i.softbank.jp>
2021-09-25 22:55:04 +0900

    アイテム音が鳴るようにscriptを変更

--- Assets/Scenes/Item.cs
+++ Assets/Scenes/Item.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    public AudioClip Get;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
+            AudioSource.PlayClipAtPoint(Get, transform.position);
             Destroy(this.gameObject);
         }
     }
